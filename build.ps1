d5b01856f9edd857eb5f1bcac040bb055ade728b
Emrys MacInally <emrys@macinally.de>
2024-10-27 14:27:36 +0100

    Refactor build script to use PSScriptRoot for Octopus.Client DLL paths

--- build.ps1
+++ build.ps1
@@ -31,10 +31,10 @@ if ($Bootstrap.IsPresent) {
         Import-Module -Name PSDepend -Verbose:$false
         Invoke-PSDepend -Path './requirements.psd1' -Install -Import -Force -WarningAction SilentlyContinue
 
-        $octoClient = (Get-ChildItem $env:DependencyFolder\dependencies\Octopus.Client.* | Sort-Object -Property @{e = { [version]$_.name.replace("Octopus.Client.", "") } } -Descending | Select-Object -First 1).fullname;
+        $octoClient = (Get-ChildItem $PSScriptRoot\dependencies\Octopus.Client.* | Sort-Object -Property @{e = { [version]$_.name.replace("Octopus.Client.", "") } } -Descending | Select-Object -First 1).fullname;
         Write-Host $octoClient
-        Copy-Item $octoClient\lib\net462\Octopus.Client.dll $env:DependencyFolder\OctopusDeploy\Lib\Desktop\Octopus.Client.dll -Force -Verbose;
-        Copy-Item $octoClient\lib\netstandard2.0\Octopus.Client.dll $env:DependencyFolder\OctopusDeploy\Lib\Core\Octopus.Client.dll -Force -Verbose
+        Copy-Item $octoClient\lib\net462\Octopus.Client.dll $PSScriptRoot\OctopusDeploy\Lib\Desktop\Octopus.Client.dll -Force -Verbose;
+        Copy-Item $octoClient\lib\netstandard2.0\Octopus.Client.dll $PSScriptRoot\OctopusDeploy\Lib\Core\Octopus.Client.dll -Force -Verbose
 
     }
 
