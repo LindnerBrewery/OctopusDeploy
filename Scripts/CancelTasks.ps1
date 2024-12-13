7aff8b523295b0a817425ad2366808bcc59c6082
Emrys MacInally <emrys@macinally.de>
2024-12-13 14:34:48 +0100

    +semver: skip skip ci]

---
+++ Scripts/CancelTasks.ps1
@@ -0,0 +1,7 @@
+﻿$release = Get-Release -Project 'Update RIS DB Schema' -Version 2024.12.11.3518
+$tasks = Get-Task -Regarding $release
+
+$repo = Get-OctopusRepositoryObject
+foreach ($task in $tasks) {
+    $repo._repository.Tasks.Cancel($task)
+}
